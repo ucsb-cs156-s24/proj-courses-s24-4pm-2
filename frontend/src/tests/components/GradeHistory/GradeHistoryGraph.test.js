b646d4dc5397a57de96fb7a513dea89b722794ac
evanja57 <easakawa@ucsb.edu>
2024-05-30 00:34:17 -0700

    ea-making sure everything is up to date and formatted correctly

--- frontend/src/tests/components/GradeHistory/GradeHistoryGraph.test.js
+++ frontend/src/tests/components/GradeHistory/GradeHistoryGraph.test.js
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import {
   oneQuarterCourse,
   twoQuarterCourse,
@@ -13,7 +12,6 @@ import {
   createCompleteGradeData,
   groupDataByQuarterAndInstructor,
 } from "main/components/GradeHistory/GradeHistoryGraph";
-import { Container } from "react-bootstrap";
 
 const mockedNavigate = jest.fn();
 
@@ -94,7 +92,7 @@ describe("Grade history tests", () => {
   });
 
   test("Correctly outputs data for one quarter", async () => {
-    const { container } = render(
+    render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
           <GradeHistoryGraph gradeHistory={oneQuarterCourse} />
@@ -102,12 +100,19 @@ describe("Grade history tests", () => {
       </QueryClientProvider>,
     );
 
+    const gradeHistoryGraphsContainer = screen.getByTestId(
+      "grade-history-graphs",
+    );
+
     await waitFor(() => {
-      const bars = container.querySelectorAll(".recharts-rectangle");
+      const bars = gradeHistoryGraphsContainer.querySelectorAll(
+        ".recharts-rectangle",
+      );
       expect(bars.length).toBe(7);
     });
 
-    const allWrappers = container.querySelectorAll(".recharts-wrapper");
+    const allWrappers =
+      gradeHistoryGraphsContainer.querySelectorAll(".recharts-wrapper");
     expect(allWrappers).toHaveLength(1);
     const element = allWrappers[0];
     expect(element).toBeInTheDocument();
@@ -115,8 +120,6 @@ describe("Grade history tests", () => {
     fireEvent.mouseOver(element, { clientX: 200, clientY: 200 });
 
     expect(element).toBeVisible();
-    //const tooltipItems = element.querySelectorAll('recharts-tooltip-item');
-    //console.log(Array.from(tooltipItems).map(item => item.textContent));
   });
 });
 
@@ -195,7 +198,6 @@ describe("groupDataByQuarterAndInstructor", () => {
 
     // Check that the keys of the grouped data are correct
     const keys = Object.keys(result);
-    console.log(result);
     expect(keys).toEqual([
       "20222-LOKSHTANOV D",
       "20221-SINGH A K",
